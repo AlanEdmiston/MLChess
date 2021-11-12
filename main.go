ef687e2e18d508edb5861fc3a706931cdfa85396
AlanEdmiston <alan.edmiston@gmail.com>
2021-11-12 00:18:55 +0000

    made move and board string representations readable by FEN and PGN readers

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 type player struct {
@@ -12,21 +11,20 @@ type player struct {
 
 func main() {
 	state := NewBoard()
-	print(state.ToString())
 
 	for i := 0; i < 11; i++ {
-		print(time.Now().Unix())
-		print("-------------------------------------------------\n")
 		tree := createRoot(4, White, &state, Player1)
 		nextMove := tree.nextMove
-		print(fmt.Sprint(nextMove.ToString()))
+		print(i + 1)
+		print(". ")
+		print(fmt.Sprint(nextMove.lastMoveString))
+		print(" ")
 		state = *nextMove
 
-		print(time.Now().Unix())
-		print("-------------------------------------------------\n")
 		tree = createRoot(4, Black, &state, Player2)
 		nextMove = tree.nextMove
-		print(fmt.Sprint(nextMove.ToString()))
+		print(fmt.Sprint(nextMove.lastMoveString))
+		print(" ")
 		state = *nextMove
 	}
 }
